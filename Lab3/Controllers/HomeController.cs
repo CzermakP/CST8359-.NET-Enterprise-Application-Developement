6a6f251352537a3a6cac75ff5be14b81bba69e7a
Patrick Czermak <45134641+CzermakP@users.noreply.github.com>
2021-06-10 22:10:52 -0400

    Initial commit of Lab 3

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        public IActionResult Count()
+        {
+            ViewBag.theCount = Request.Form["theCount"];
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson( Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return Error();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
